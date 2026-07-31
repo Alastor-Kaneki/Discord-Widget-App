8862c9d431b30bd95b481a9164f7e27c29ee3776
Alastor Kaneki <170276077+Alastor-Kaneki@users.noreply.github.com>
2026-07-31 16:56:23 -0400

    Add animated rainbow message input

---
+++ app/src/main/java/com/alastorkaneki/discordwidget/RainbowEditText.java
@@ -0,0 +1,54 @@
+package com.alastorkaneki.discordwidget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatEditText;
+
+public final class RainbowEditText extends AppCompatEditText {
+    private RainbowOutlineDrawable outline;
+
+    public RainbowEditText(Context context) {
+        super(context);
+        initialize(context);
+    }
+
+    public RainbowEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialize(context);
+    }
+
+    public RainbowEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initialize(context);
+    }
+
+    private void initialize(Context context) {
+        outline = new RainbowOutlineDrawable(context, 16f, 2f);
+    }
+
+    @Override
+    protected void onSizeChanged(int width, int height, int oldWidth, int oldHeight) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight);
+        outline.setBounds(0, 0, width, height);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        outline.draw(canvas);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        outline.start();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        outline.stop();
+        super.onDetachedFromWindow();
+    }
+}
